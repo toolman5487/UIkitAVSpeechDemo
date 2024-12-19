b35060eea2a9fa041245cd926733cbe1e80a0dca
Willy <willy548798@gmail.com>
2024-12-19 16:30:16 +0800

    Initial Commit

---
+++ HelloAVSpeech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloAVSpeech
+//
+//  Created by Willy Hsu on 2024/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
